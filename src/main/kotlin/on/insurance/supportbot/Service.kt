33774265cd73267df568d06acb40b7b28aac31ce
Karimxon <omonxonovsaidkarim1998@gmail.com>
2022-08-18 20:53:20 +0500

    creat groupByOperatorId func

--- src/main/kotlin/on/insurance/supportbot/Service.kt
+++ src/main/kotlin/on/insurance/supportbot/Service.kt
@@ -1,14 +1,9 @@
 package on.insurance.supportbot
 
 import on.insurance.supportbot.teligram.Group
-import on.insurance.supportbot.teligram.Language
 import on.insurance.supportbot.teligram.MessageEntity
 import on.insurance.supportbot.teligram.User
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.data.jpa.repository.Query
-import org.springframework.expression.spel.ast.Operator
 import org.springframework.stereotype.Service
-import org.telegram.telegrambots.meta.api.objects.Update
 
 interface UserService {
     fun getUser(chatId: Long): User
@@ -19,20 +14,13 @@ interface UserService {
 interface GroupService {
     fun update(group: Group): Group
     fun getGroupByUserId(userId: Long): Group
+    fun getGroupByOperatorId(operatorId: Long): Group
+    fun connectOperator(operator: User):Group?
 
-//    fun connectOperator(operator: User):Group
-//    operator= null
-//    and isActive = true
-//    and group.Language=operator.Language
-//    order by date 1
 
 }
 interface MessageService{
     fun creat(message: String,group: Group,user: User)
-
-//    fun getUserMessage(user: User,group: Group):List<MessageEntity>
-//    order date, readed=false,
-//    kiyin readed=true qilib quyasizlar
 }
 
 @Service
@@ -51,13 +39,19 @@ class GroupServiceImpl(
     val groupRepository: GroupRepository,
     val userRepository: UserRepository,
 ) : GroupService {
-
     override fun update(group: Group): Group {
         return groupRepository.save(group)
     }
-
     override fun getGroupByUserId(userId: Long): Group {
     return groupRepository.findByUserIdAndDeleted(userId).run { this } ?: createGroup(userId)
+    }
+    override fun getGroupByOperatorId(operatorId: Long): Group {
+        return groupRepository.findByOperatorIdAndDeleted(operatorId).run { this } ?: Group(  )
+    }
+
+
+    override fun connectOperator(operator: User): Group? {
+       return  groupRepository.getOperator(operator.language)?:throw RuntimeException("bunday group yoq")
     }
 
     fun createGroup(userId: Long): Group {
