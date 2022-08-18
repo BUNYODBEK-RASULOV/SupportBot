c352a209442c787a9c2e2d00d6f6050c42c629e3
BUNYODBEK-RASULOV <7777777tegir@gmail.com>
2022-08-18 11:37:37 +0500

    created lanfaude

--- src/main/kotlin/on/insurance/supportbot/teligram/BotService.kt
+++ src/main/kotlin/on/insurance/supportbot/teligram/BotService.kt
@@ -4,13 +4,17 @@ package on.insurance.supportbot.teligram
 import on.insurance.supportbot.UserService
 import org.springframework.stereotype.Service
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.DeleteMessage
 import org.telegram.telegrambots.meta.api.objects.Update
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException
+
 
 @Service
 class BotService(
     val myBot: MyBot,
-    val userService: UserService
+    val userService: UserService,
 ) {
 
     public fun massage(update: Update) {
@@ -20,19 +24,14 @@ class BotService(
             chatId = message.chatId
             text = message.text
         }
-        println(chatId)
         var user = userService.getUser(chatId)
         var botStep = user.botStep
 
+
         when(botStep){
             BotStep.START->{
-                sendMassage(chatId,"tilni tanlang")
+                sendMassage(chatId,"tilni tanlang",languageButtons())
                 user.botStep=BotStep.LANGUAGE
-                userService.update(user)
-            }
-            BotStep.LANGUAGE->{
-                sendMassage(chatId,"kontakni yuboring")
-                user.botStep=BotStep.CONTACT
                 userService.update(user)
             }
             BotStep.CONTACT->{
@@ -42,10 +41,58 @@ class BotService(
 
     }
 
+    fun inline(update: Update){
+        var chatId: Long = 0
+        var data = ""
+        update.run {
+            chatId=callbackQuery.message.chatId
+            data=callbackQuery.data
+        }
+        var user = userService.getUser(chatId)
+        var botStep = user.botStep
+
+        when(botStep){
+            BotStep.LANGUAGE->{
+                myBot.deleteMassage(chatId,update)
+                sendMassage(chatId,"kontakni yuboring")
+                user.botStep=BotStep.CONTACT
+                user.language= Language.valueOf(data)
+                userService.update(user)
+            }
+        }
+    }
+
+
+    fun sendMassage(chatId: Long, text: String,inlineKeyboardMarkup: InlineKeyboardMarkup?) {
+        val sendMessage = SendMessage(chatId.toString(), text)
+        inlineKeyboardMarkup?.run { sendMessage.replyMarkup=this }
+        sendMessage.enableMarkdown(true)
+        myBot.execute(sendMessage) ?: throw TelegramApiException("xatolik")
+    }
+
+
 
     fun sendMassage(chatId: Long, text: String) {
         val sendMessage = SendMessage(chatId.toString(), text)
         sendMessage.enableMarkdown(true)
         myBot.execute(sendMessage) ?: throw TelegramApiException("xatolik")
+    }
+
+    fun languageButtons():InlineKeyboardMarkup{
+        val inlineKeyboardMarkup = InlineKeyboardMarkup()
+
+        var keyboardButtons= mutableListOf<InlineKeyboardButton>()
+
+        var buttons = listOf<Language>(Language.UZ, Language.RU, Language.ENG)
+        buttons.forEach {
+            val inlineKeyboardButton = InlineKeyboardButton()
+            inlineKeyboardButton.text =it.name
+            inlineKeyboardButton.callbackData = it.name
+            keyboardButtons.add(inlineKeyboardButton);
+        }
+        val rowList: MutableList<List<InlineKeyboardButton>> = ArrayList()
+        rowList.add(keyboardButtons)
+        inlineKeyboardMarkup.setKeyboard(rowList);
+        return inlineKeyboardMarkup
     }
 }
