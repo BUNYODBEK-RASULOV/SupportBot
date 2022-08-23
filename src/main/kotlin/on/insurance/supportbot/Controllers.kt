184b1c8a92161583c9de9ab0779c7480e833b30d
BUNYODBEK-RASULOV <7777777tegir@gmail.com>
2022-08-23 15:16:38 +0500

    git Labga

--- src/main/kotlin/on/insurance/supportbot/Controllers.kt
+++ src/main/kotlin/on/insurance/supportbot/Controllers.kt
@@ -1,10 +1,13 @@
 package on.insurance.supportbot
 
 
-import on.insurance.supportbot.teligram.Group
 import on.insurance.supportbot.teligram.MessageEntity
 import on.insurance.supportbot.teligram.User
+import org.springframework.util.FileCopyUtils
 import org.springframework.web.bind.annotation.*
+import java.io.FileInputStream
+import java.io.IOException
+import javax.servlet.http.HttpServletResponse
 
 
 @RestController
@@ -35,4 +38,32 @@ class OperatorController(
 
     @GetMapping("messageList/{groupId}")
     fun getAllMessageList(@PathVariable groupId:Long):List<MessageEntity> = messageService.getAllMessageByGroupId(groupId)
+
+}
+
+@RestController
+@RequestMapping("/api/v1/document")
+class DocumentController(
+    val messageRepository: MessageRepository
+){
+    val document: String ="document"
+
+//
+//    @GetMapping("getFromFileSystem/{id}")
+//    @Throws(IOException::class)
+//    fun getFromFileSystem(@PathVariable id: Long, response: HttpServletResponse) {
+//        val byId: Optional<Attachment> = attachmentRepository.findById(id)
+//        if (byId.isPresent()) {
+//            val attachment: Attachment = byId.get()
+//            response.setHeader(
+//                "Content-Disposition",
+//                "attachment; file=\"" + attachment.getFileOriginalName().toString() + "\""
+//            )
+//            response.contentType = attachment.getContentType()
+//            val fileInputStream = FileInputStream(document.toString() + "/" + attachment.getName())
+//            FileCopyUtils.copy(fileInputStream, response.outputStream)
+//        }
+//    }
+
+
 }
